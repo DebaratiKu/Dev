9bcba3f25391216d1eae3e6e61b1dbbc833ac253
debarati.k1693@gmail.com <debarati.k1693@gmail.com>
2025-08-19 09:53:17 +0000

    development

--- models/Hub/hub_customer.sql
+++ models/Hub/hub_customer.sql
@@ -1,8 +1,13 @@
 {{ config(materialized='incremental',
 unique_key='CUSTOMER_HK') }}
 
-SELECT customer_id,
+with src as(SELECT customer_id,
 customer_hk,
 load_dts,
 source
- FROM {{ ref('stg_customers') }}
+ FROM {{ ref('stg_customers') }})
+
+select * from src
+{%if is_incremental() %}
+where customer_hk not in(select customer_hk from {{this}})
+{%endif%}
--- models/Link/link_customer_order.sql
+++ models/Link/link_customer_order.sql
@@ -1,11 +1,16 @@
 {{ config(materialized='incremental',
 unique_key='CUSTOMER_ORDER_HK') }}
 
-SELECT 
+with src as (SELECT 
 CUSTOMER_ORDER_HK,
 CUSTOMER_HK,
 ORDER_HK,
 ORDER_ID,
 LOAD_DTS,
 SOURCE
- FROM {{ ref('stg_orders') }}
+ FROM {{ ref('stg_orders') }})
+
+select * from src
+{%if is_incremental() %}
+where CUSTOMER_ORDER_HK not in(select CUSTOMER_ORDER_HK from {{this}})
+{%endif%} 
